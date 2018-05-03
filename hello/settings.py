9b539d34a708ab719f47b1f9ee1476a46af2ddea
Prince Singh <pk089090@gmail.com>
2018-05-04 02:28:49 +0530

    for heroku

--- hello/settings.py
+++ hello/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rm_552j@a78!n=dx&$1q@+bb4-r5kpgge3v^g4!8nmg79t978('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =False
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com',]
 
 
 # Application definition
